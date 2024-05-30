083d55c61ad263c429ddf697a14b88da7bbc5d80
George Pyne <georgepyne@Georges-MacBook-Pro.local>
2024-05-30 16:42:59 -0700

    argparse add

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-        description="List STAC query bounds: min_lon, min_lat, max_lat, max_lon"
+        description="List STAC query longitude and latitude bounds: min_lon, min_lat, max_lat, max_lon"
     )
     parser.add_argument("-b", "--bounds", type=str)
+    parser.add_argument("-t", "--time", type=str)
     args = parser.parse_args()
     bounds = args.bounds
+    time = args.time
     print(bounds)
+    print(time)
+    print(f"Query Microsoft Planetary Computer:\nbounds = {bounds}\ntime = {time}")
 
 
 if __name__ == "__main__":
