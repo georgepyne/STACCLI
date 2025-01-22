8111a06a5d03fff1e56983df7ac60218976df85e
George Pyne <georgepyne@hotmail.co.uk>
2025-01-21 18:17:31 -0800

    feat: add notebook

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import argparse
 import logging
 import os
 import sys
-from typing import List, cast
-
 import mercantile
+from typing import List, cast
 from osgeo import gdal
 from pyproj import Transformer
 from shapely.geometry import box
-
 from src.stac.planetary_computer import query_planetary_computer_stac
 from src.stac.stac_parameter_parser import parse_bbox, parse_time_window
 from src.stac.stac_utils import get_bbox_and_footprint, order_stac, write_stac_meta
@@ -56,8 +54,21 @@ def main() -> None:
     parser.add_argument("-a", "--asset", help="Asset id to query STAC", type=str)
     gdal.UseExceptions()
     try:
+        from pyproj import datadir
+
+        os.environ["PROJ_LIB"] = (
+            "C:\\Users\\Geo\\miniconda3\\envs\\STACCLI\\Library\\share\\proj"
+        )
+        print(os.environ.get("PROJ_LIB"))
+
+        print(datadir.get_data_dir())
+
         # parse args
         args = parser.parse_args()
+        datadir.set_data_dir(
+            "C:\\Program Files\\PostgreSQL\\13\\share\\contrib\\postgis-3.0\\proj\\proj.db"
+        )
+        print(datadir.get_data_dir())
         bounds = parse_bbox(args.bounds)
         time = parse_time_window(args.time)
         collection_id = args.collection
@@ -70,9 +81,7 @@ def main() -> None:
             max_tile = mercantile.minmax(z)[1]  # get max tile coord for zoom level
         else:
             raise IndexError(f"Invalid zoom level: {z}")
-        if (
-                0 <= x >= max_tile or 0 <= y >= max_tile
-        ):  # coord values valid at zoom level
+        if 0 <= x >= max_tile or 0 <= y >= max_tile:  # coord values valid at zoom level
             raise IndexError(
                 f"Invalid tile coordinate for zoom level {z}: x={x} y={y}."
             )
@@ -117,131 +126,153 @@ def main() -> None:
         assets = asset.split(",")
         asset_bands = {assets[i]: i for i in range(len(assets))}
         cog_urls = []
-        for a in assets:
-            cog_urls = cog_urls + [
-                i["assets"][a]["href"] for i in [ordered_features[2]]
-            ]
-        # cogs = [rasterio_open(i) for i in cog_urls]
 
-        outvrt = (
-            "/vsimem/stacked.vrt"  # /vsimem is special in-memory virtual "directory"
-        )
-        outtif = "/Users/geo/Desktop/test.tif"
-        tifs = cog_urls
+        for feat in ordered_features:
+            for a in assets:
+                cog_urls = cog_urls + [i["assets"][a]["href"] for i in [feat]]
+            # cogs = [rasterio_open(i) for i in cog_urls]
+            id = feat["id"]
 
-        # left, bottom, right, top = bounds
+            outvrt = "/vsimem/stacked.vrt"  # /vsimem is special in-memory virtual "directory"
+            # outtif = "/Users/geo/Desktop/test.tif"
+            # tifs = cog_urls
 
-        polygon = box(*[left, bottom, right, top])
+            # left, bottom, right, top = bounds
 
-        outds = gdal.BuildVRT(outvrt, cog_urls, separate=True)
+            polygon = box(*[left, bottom, right, top])
 
-        outds = gdal.Translate(outtif, outds)
+            os.environ["PROJ_LIB"] = (
+                "C:\\Users\\Geo\\miniconda3\\envs\\STACCLI\\Library\\share\\proj"
+            )
 
-        # TODO:
-        #### Cloud cover func ######## sprint 1
-        # get band rgb as 256*256 array from translate_ds
-        # save png to tmp
-        # read with cloudgan remove cc
-        # cloudgan interpolate (nodataMask, cloudMask if cloudMaskInterpolate)
-        # translate mask to cloud band
-        ##############################
+            outds = gdal.BuildVRT(outvrt, cog_urls, separate=True)
 
-        #### change detection func #### sprint 1
-        # change detect with CSA-CDGAN
-        # translate mask to change detection band
-        ###############################
+            # outds = gdal.Translate(outvrt, outds)
 
-        #### object detection func #### sprint 1
-        # object detect with torchGeo
-        # rasterToVector objects detected
-        # write geoJson from STAC item, change geometry to rasterToVector
-        ###############################
+            # ST_AddBand(raster torast, raster[], ST_MakeEmptyRaster(integer
+            # width, integer
+            # height, float8
+            # upperleftx, float8
+            # upperlefty, float8
+            # scalex, float8
+            # scaley, float8
+            # skewx, float8
+            # skewy, integer
+            # srid = unknown) ...
+            # fromrasts, integer
+            # fromband = 1, integer
+            # torastindex = at_end)
+            # )
 
-        #### land cover pred #### sprint 2
-        # land cover pred with torchGeo (or other)
-        # translate mask to change detection band
-        ###############################
+            # TODO:
+            #### Cloud cover func ######## sprint 1
+            # get band rgb as 256*256 array from translate_ds
+            # save png to tmp
+            # read with cloudgan remove cc
+            # cloudgan interpolate (nodataMask, cloudMask if cloudMaskInterpolate)
+            # translate mask to cloud band
+            ##############################
 
-        ### change report func ####### sprint 2
-        # world2pixel ssi of previous image
-        # Change detection %s, area
-        # objects in cd area
-        # landcover change in cd area
-        # cloud cover interpolated
-        # no data interpolated
+            #### change detection func #### sprint 1
+            # change detect with CSA-CDGAN
+            # translate mask to change detection band
+            ###############################
 
-        ### build image storage ### sprint 3/4
-        # upload world2pixel ratser to postGIS
-        # create stac storage (unprocessed, processed) in supabase upload storage
-        # unprocessed storage event -> queue process images to processed storage
-        # storage processed storage event ->  queue pgstac update
-        # deploy stac api service
-        # create aoi for continuous monitoring
-        # acl
-        # build endpoint -> mpc -> pgstac & supabase storage
+            #### object detection func #### sprint 1
+            # object detect with torchGeo
+            # rasterToVector objects detected
+            # write geoJson from STAC item, change geometry to rasterToVector
+            ###############################
 
-        ## UI ## sprint 5/6
-        # show map with datrange with layers:
-        # rgb, objects, landcover, cd, cc
-        # show report dashboard
-        # create, update, delete aoi
-        # acl
+            #### land cover pred #### sprint 2
+            # land cover pred with torchGeo (or other)
+            # translate mask to change detection band
+            ###############################
 
-        # for i in range(1, 3):
-        #
-        #     gdal.FillNodata(
-        #         ref.GetRasterBand(i),
-        #         None,
-        #         1000,
-        #         2,
-        #         {"INTERPOLATION": "NEAREST"},
-        #     )
+            ### change report func ####### sprint 2
+            # world2pixel ssi of previous image
+            # Change detection %s, area
+            # objects in cd area
+            # landcover change in cd area
+            # cloud cover interpolated
+            # no data interpolated
 
-        format =
-        cutlineWKT =
-        cutlineSRS = "epsg:4326",
-        cropToCutline = True,
-        # cutlineBlend
-        dstNodata = 0,
-        resampleAlg = "bilinear",
-        width = 256,
-        height = 256,
+            ### build image storage ### sprint 3/4
+            # upload world2pixel ratser to postGIS
+            # create stac storage (unprocessed, processed) in supabase upload storage
+            # unprocessed storage event -> queue process images to processed storage
+            # storage processed storage event ->  queue pgstac update
+            # deploy stac api service
+            # create aoi for continuous monitoring
+            # acl
+            # build endpoint -> mpc -> pgstac & supabase storage
 
-        gdal.WarpOptions()
+            ## UI ## sprint 5/6
+            # show map with datrange with layers:
+            # rgb, objects, landcover, cd, cc
+            # show report dashboard
+            # create, update, delete aoi
+            # acl
 
-        gdal.Warp(
-            "/Users/geo/Desktop/testWarp.tif",
-            outtif,
-            # errorThreshold
-            format="COG",
-            cutlineWKT=polygon.wkt,
-            cutlineSRS="epsg:4326",
-            cropToCutline=True,
-            # cutlineBlend
-            dstNodata=0,
-            resampleAlg="bilinear",
-            width=256,
-            height=256,
-        )
+            # for i in range(1, 3):
+            #
+            #     gdal.FillNodata(
+            #         ref.GetRasterBand(i),
+            #         None,
+            #         1000,
+            #         2,
+            #         {"INTERPOLATION": "NEAREST"},
+            #     )
 
-        output_types = [gdal.GDT_Byte, gdal.GDT_UInt16, gdal.GDT_Float32]
+            # warp_objects
+            # gdal.WarpOptions(**wo)
 
-        # Define output format and options
-        options = gdal.TranslateOptions(
-            format="PNG",
-            bandList=[1, 2, 3],
-            creationOptions=["WORLDFILE=YES"],
-            outputType=output_types[1],
-            width=256,
-            height=256,
-        )
+            xres = outds.RasterXSize
+            yres = outds.RasterYSize
 
-        gdal.Translate("/Users/geo/Desktop/testWarp.png", outtif, options=options)
+            outds = gdal.Translate(
+                f"{file_path}/{id}.tif",
+                outds,
+                # dstNodata=-1,
+                resampleAlg="bilinear",
+                format="COG",
+                width=xres,
+                height=yres,
+            )
 
-        # # validate cog
-        # validate_cloud_optimized_geotiff.validate(
-        #     "/Users/geo/Desktop/testWarp.tif", full_check=True
-        # )
+            gdal.Warp(
+                f"{file_path}/{id}.tif",
+                outvrt,
+                # errorThreshold
+                format="COG",
+                cutlineWKT=polygon.wkt,
+                cutlineSRS="epsg:4326",
+                cropToCutline=True,
+                # cutlineBlend
+                dstNodata=-1,
+                resampleAlg="bilinear",
+                width=xres,
+                height=yres,
+            )
+
+            output_types = [gdal.GDT_Byte, gdal.GDT_UInt16, gdal.GDT_Float32]
+
+            # Define output format and options
+            # options = gdal.TranslateOptions(
+            #     format="PNG",
+            #     bandList=[1, 2, 3],
+            #     creationOptions=["WORLDFILE=YES"],
+            #     outputType=output_types[1],
+            #     width=256,
+            #     height=256,
+            # )
+
+            # gdal.Translate("C:/Users/Geo/Desktop/testWarp.png", outtif, options=options)
+
+            # # validate cog
+            # validate_cloud_optimized_geotiff.validate(
+            #     "/Users/geo/Desktop/testWarp.tif", full_check=True
+            # )
 
         # # reproject bounds
         epsg = items["features"][0]["properties"][
